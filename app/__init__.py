b15185a2896e15a4c5c11ced123944c145c6a86d
8kyu <guillemsenabre@gmail.com>
2024-02-25 22:47:34 +0100

    feat: blueprint registered

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from scrapper.scrapper_routes import scrapper_blueprint
 
 def create_app():
   app = Flask(__name__)
+
+  # Register blueprints
+  app.register_blueprint(scrapper_blueprint)
+
   return app
