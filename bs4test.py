abe6b03ab894afd6ba78c7690130e2898251d0b0
8kyu <guillemsenabre@gmail.com>
2024-02-25 08:52:22 +0100

    Refactor: refactor into a method

--- bs4test.py
+++ bs4test.py
@@ -1,17 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.google.com/search"
-params = {"q": "boring stuff"}  # add "hl":"en" to get english results
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
-}
-tags = BeautifulSoup(
-    requests.get(url, params=params, headers=headers).content, "html.parser"
-)
+def crawl_data():
 
-num_links = 5
+  # Dyanimc query
+  query = input(f"Crawl data from: ")
 
-for index in range(num_links):
-    a = tags.select("a:has(h3)")[index]
-    print(a["href"])
+  # Google URL, can Change to other browsers as well
+  url = "https://www.google.com/search"
+
+  # input parameters (query, ...)
+  params = {"q": str(query)}  # add "hl":"en" to get english results
+
+  headers = {
+      "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
+  }
+  
+  # Response from the search (tags is a bs4 object)
+  tags = BeautifulSoup(
+      requests.get(url, params=params, headers=headers).content, "html.parser"
+  )
+
+  # Limit of returned links
+  num_links = 5
+
+  # loop to retrieve the link from the <a> tag
+  for index in range(num_links):
+      a = tags.select("a:has(h3)")[index]
+      print(a["href"])  
+  
+  return "Search completed!"
+
+if __name__=="__main__":
+   crawl_data()
+
