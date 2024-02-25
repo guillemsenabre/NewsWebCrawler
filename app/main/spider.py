4b037405bccf42c980f2a75df90e09705d11f3cf
8kyu <guillemsenabre@gmail.com>
2024-02-25 21:46:29 +0100

    feat: basic spider class returns links and h3

--- app/main/spider.py
+++ app/main/spider.py
@@ -6,14 +6,22 @@ class BasicSpider():
         # Each object has is own identifier
         self.name = str(name)
 
-    def get_links(self, query):
-    
-        # The input query is comming from the frontend
-
         # Google URL, can Change to other browsers as well
         #although Google has one of the best search engines right now
-        url = "https://www.google.com/search"
+        self.url = "https://www.google.com/search"
 
+    def get_data(self):
+
+        query = input("Input query: ")
+        
+        response_tree = self._get_response(self.url, query)
+        links_and_h3 = self._get_links_and_headers(response_tree)
+
+        #links = [a["href"] for a in website_tree.select("a:has(h3)") if "href" in a.attrs]
+        print(links_and_h3)
+        return "finished"
+
+    def _get_response(self, url, query):
         # pass form (query) data and customize web page behaviour
         # add "hl":"en" to get english results
         params = {"q": str(query), "sort": "by_date"} 
@@ -26,11 +34,30 @@ class BasicSpider():
         }
         
         # Response from the search (tags is a bs4 object)
-        tags = BeautifulSoup(
+        website_tree = BeautifulSoup(
             requests.get(url, params=params, headers=headers).content, "html.parser"
         )
 
-        # Limit of returned links
-        links = [a["href"] for a in tags.select("a:has(h3)") if "href" in a.attrs]
-        
-        return links
+        return website_tree
+
+
+    def _get_links_and_headers(self, response):
+        # Dictionary to store header3 and the href link
+        links_and_h3 =  {}
+
+        # This counter is needed since the not all anchors contain 'href' or 'h3'
+        index = 0
+
+        # Loops over all anchors
+        for anchor in response.find_all('a'):
+            
+            # Extracts only the anchors that contain href AND h3
+            if anchor.find_all('h3') and "href" in anchor.attrs:
+
+                # Builds the dictionary with the extracted data
+                links_and_h3[f"pair{index}"] = anchor.find('h3').text.strip(), anchor['href']
+                
+                index += 1
+
+        return links_and_h3
+
