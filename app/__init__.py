aa9714a288d3aa0d025f46606699a4ec138f00c4
8kyu <guillemsenabre@gmail.com>
2024-02-28 20:19:27 +0100

    PROJECT RESTRUCTURE

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from app.scrapper.scrapper_routes import scrapper_blueprint
+from app.blueprints.main.routes import main_bp
 
 def create_app():
   app = Flask(__name__)
 
   # Register blueprints
-  app.register_blueprint(scrapper_blueprint)
+  app.register_blueprint(main_bp)
+
 
   return app
--- app/scrapper/scrapper_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-scrapper_blueprint = Blueprint('scrapper_blueprint', __name__)
-
-@scrapper_blueprint.route('/scrap_data')
-def scrap_data():
-  pass
-
-
