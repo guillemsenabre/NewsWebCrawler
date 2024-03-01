3181e603494b977e7ab05a57efb519f92d387713
8kyu <guillemsenabre@gmail.com>
2024-03-01 21:23:52 +0100

    feat: text transition when search submitted

---
+++ app/static/main/js/fadeOutText.js
@@ -0,0 +1,16 @@
+text = document.getElementById("mainTextContainer");
+inputBox = document.getElementById("inputBox");
+let isVisible = true;
+
+inputBox.addEventListener('submit', function() {
+  if (isVisible) {
+    text.classList.add('disappear');
+    text.classList.remove('appear');
+    isVisible = false;
+  }
+  else {
+    text.classList.add('appear');
+    text.classList.remove('disappear');
+    isVisible = true;
+  }
+});
